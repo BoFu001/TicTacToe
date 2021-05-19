9cc12bd269fa7d0f2594d650a3040a6c373d14fa
bofu <bofu2013@gmail.com>
2021-05-19 16:09:55 +0200

    added initializeGameManager

--- app/src/test/java/com/bofu/tictactoetdd/GameManagerTest.kt
+++ app/src/test/java/com/bofu/tictactoetdd/GameManagerTest.kt
@@ -1,27 +1,32 @@
 package com.bofu.tictactoetdd
 
 import com.google.common.truth.Truth
+import org.junit.Before
 import org.junit.Test
 
 class GameManagerTest {
 
+    private lateinit var gameManager: GameManager
+
+    @Before
+    fun initializeGameManager(){
+        gameManager = GameManager()
+    }
+
     @Test
     fun initialPlayerTest(){
-        val gameManager = GameManager()
         val currentPlayer = gameManager.currentPlayer
         Truth.assertThat(currentPlayer).isEqualTo(1)
     }
 
     @Test
     fun initialPlayerMarkTest(){
-        val gameManager = GameManager()
         val currentPlayerMark = gameManager.currentPlayerMark
         Truth.assertThat(currentPlayerMark).isEqualTo("X")
     }
 
     @Test
     fun PlayTest(){
-        val gameManager = GameManager()
         gameManager.play()
         val currentPlayerMark = gameManager.currentPlayerMark
         Truth.assertThat(currentPlayerMark).isEqualTo("O")
@@ -29,7 +34,6 @@ class GameManagerTest {
 
     @Test
     fun getGameState(){
-        val gameManager = GameManager()
         val state = gameManager.state
         state.forEach{row -> row
             row.forEach { column -> column
