9e26f2981b32e0085fd5ad9581176c1c4a98b316
bofu <bofu2013@gmail.com>
2021-05-19 14:57:51 +0200

    created play function

--- app/src/main/java/com/bofu/tictactoetdd/GameManager.kt
+++ app/src/main/java/com/bofu/tictactoetdd/GameManager.kt
@@ -1,8 +1,14 @@
 package com.bofu.tictactoetdd
 
 class GameManager {
+
     var currentPlayer = 1
 
     val currentPlayerMark: String
         get() = if (currentPlayer == 1) "X" else "O"
+
+
+    fun play() {
+        currentPlayer++
+    }
 }
