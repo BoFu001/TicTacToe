80291eed97d5978ba2ebdc32803a4c8f0e456180
bofu <bofu2013@gmail.com>
2021-05-19 18:01:06 +0200

    added detectWinCaseTest

--- app/src/test/java/com/bofu/tictactoetdd/GameManagerTest.kt
+++ app/src/test/java/com/bofu/tictactoetdd/GameManagerTest.kt
@@ -52,4 +52,53 @@ class GameManagerTest {
             }
         }
     }
+
+    @Test
+    fun detectWinCaseTest(){
+        val state1 = gameManager.state
+        for (column in state1[0].indices){
+            state1[0][column] = gameManager.currentPlayer
+        }
+        val case1 = gameManager.detectWinCase(state1)
+        Truth.assertThat(case1).isEqualTo("Horizontal")
+
+        gameManager.reset()
+
+        val state2 = gameManager.state
+        for (row in state2.indices){
+            for(column in state2[row]){
+                if (column == 0){
+                    state2[row][column] = gameManager.currentPlayer
+                }
+            }
+        }
+        val case2 = gameManager.detectWinCase(state2)
+        Truth.assertThat(case2).isEqualTo("vertical")
+
+        gameManager.reset()
+
+        val state3 = gameManager.state
+        for (row in state3.indices){
+            for(column in state3[row]){
+                if (column == row){
+                    state2[row][column] = gameManager.currentPlayer
+                }
+            }
+        }
+        val case3 = gameManager.detectWinCase(state3)
+        Truth.assertThat(case3).isEqualTo("diagonalLeft")
+
+        gameManager.reset()
+
+        val state4 = gameManager.state
+        for (row in state4.indices){
+            for(column in state4[row]){
+                if (column == state4[row].lastIndex - column){
+                    state4[row][column] = gameManager.currentPlayer
+                }
+            }
+        }
+        val case4 = gameManager.detectWinCase(state4)
+        Truth.assertThat(case4).isEqualTo("diagonalRight")
+    }
 }
